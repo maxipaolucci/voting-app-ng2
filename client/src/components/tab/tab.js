bb60b49a8511529638f96128443d898451e8976f
Maxi Paolucci <maxipaolucci@gmail.com>
2016-07-27 12:23:03 +1200

    added sass to webpack

---
+++ client/src/components/tab/tab.js
@@ -0,0 +1,3 @@
+import './tab.scss';
+
+export const tabTitle = "this is the tab title";
--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,11 @@
-let hello = name => console.log(`Hello ${name}!!!`);
+import './styles/main.scss'; //first the base stylesheet
 
-hello('Maxi');
+import io from 'socket.io-client';
+import {tabTitle} from 'components/tab/tab'; //import the component and its styles
+
+const serverPort = 3030; //the server by default is configure to listen at this port
+const socket = io(`${location.protocol}//${location.hostname}:${serverPort}`); //connect to the server
+
+socket.on('hello', msg => console.log(msg)); //receive hello event from the server
+
+console.log(tabTitle);
