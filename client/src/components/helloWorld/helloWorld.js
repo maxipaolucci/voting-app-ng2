77d0ec95cca0f286a80dca92be651b94a2159cd1
Maximiliano Paolucci <maximiliano.paolucci@vodafone.com>
2016-08-10 15:32:45 +1200

    all webpack conf and test app created

--- client/src/components/helloWorld/helloWorld.js
+++
@@ -1,8 +0,0 @@
-import './helloWorld.scss';
-
-export const helloWorldStr = "This is the hello world component.";
-
-//this fn is just defined to test the mocka testing library (see test directory)
-export const squareFn = function (value = 0) {
-  return value * value;
-};
