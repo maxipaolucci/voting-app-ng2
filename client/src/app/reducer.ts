f41df377da72f375cd50ab80e88fe84802500e6b
Maxi Paolucci <maxipaolucci@gmail.com>
2016-08-22 23:38:51 +1200

    trying to do a reducer

--- client/src/app/reducer.ts
+++ client/src/app/reducer.ts
@@ -1,10 +1,23 @@
-import {Map} from 'immutable';
+import {Map, List} from 'immutable';
+
+export interface IVote extends Map<string, any> {
+  pair : List<string>,
+  tally? : Map<string, number>
+}
+
+export interface IAppState extends Map<string, any> {
+  movies? : List<string>,
+  vote? : IVote,
+  winner? : string
+}
+
+const INITIAL_STATE : IAppState = Map<string, any>();
 
 const setState = (state, newState) => {
   return state.merge(newState);
 }
 
-export default (state = Map(), action) => {
+export default (state : IAppState = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'SET_STATE':
       return setState(state, action.state);
