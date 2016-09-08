b46eed7d54063943e1a8d10dee5ae339da9a04a8
Maxi Paolucci <maxipaolucci@gmail.com>
2016-09-08 18:55:26 +1200

    added results component

--- client/src/app/components/results/results.component.ts
+++ client/src/app/components/results/results.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output, OnInit } from '@angular/core';
+import { List, Map } from  'immutable';
+import {select} from "ng2-redux/lib/index";
+import {VottingActions} from "../../vottingActions.service";
+import {Observable} from "rxjs/Rx";
 
 
 @Component({
@@ -7,6 +11,29 @@ import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from 
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.scss']
 })
-export class ResultsComponent {
+export class ResultsComponent implements OnInit {
+  private tally : Map<string, number> = null;
+  @select(['vottingModel', 'vote', 'pair']) votePair : Observable<List<string>>;
+  @select(['vottingModel', 'vote', 'tally']) voteTally : Observable<Map<string, number>>;
 
+  constructor(private vottingActions: VottingActions) {}
+
+  ngOnInit() {
+    //we subscribe to this observable to receive the tally async
+    this.voteTally.subscribe((value : Map<string, number>) => this.tally = value );
+  }
+
+  /**
+   * Returns the amount of votes from an item in the current pair
+   * @param item (string). The item we are looking at
+   * @returns {number}
+   */
+  getVotes(item : string) : number {
+    //check for the tally to be not null (before voteTally gets a value from the server)
+    if (this.tally ) {
+      return this.tally.get(item, 0);
+    }
+
+    return 0;
+  }
 }
--- client/src/app/components/vote/vote.component.ts
+++ client/src/app/components/vote/vote.component.ts
@@ -8,6 +8,14 @@ import { Component, Input, ChangeDetectionStrategy, EventEmitter, Output } from 
   styleUrls: ['./vote.component.scss']
 })
 export class VoteComponent {
+  private isVoted = false;
   @Input() item: string;
   @Output() voted: EventEmitter<any> = new EventEmitter();
+
+  onVoted(item : string) {
+    if (!this.isVoted) {
+      this.voted.emit(item);
+      this.isVoted = true;
+    }
+  }
 }
